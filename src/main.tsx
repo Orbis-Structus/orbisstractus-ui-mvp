b528000a3fbeb0ad41d429f2a39d8c847520cc48
Bardia Alimohammadshah <bari.ams01@gmail.com>
2026-08-12 23:15:36 -0400

    Enable demo authentication in production

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,8 @@ import './index.css'
 import App from './App.tsx'
 
 async function prepare() {
-  if (import.meta.env.DEV) {
-    const { worker } = await import('./mocks/browser');
-    await worker.start({ onUnhandledRequest: 'bypass' });
-  }
+  const { worker } = await import('./mocks/browser');
+  await worker.start({ onUnhandledRequest: 'bypass' });
 }
 
 prepare().then(() => {
